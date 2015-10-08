2dac19714747bd405506d4790e8eb221960a5324
Tiao <Tiao@whatever.com>
2015-10-07 23:46:50 -0400

    Os project 2 working?

--- OsProject2/Project2.cpp
+++ OsProject2/Project2.cpp
@@ -5,22 +5,21 @@
 
 //Include libraries
 
-#include <stdlib.h>	//Exit functions for incorrect files
-#include <time.h>	//Timing
-#include <string>	//Buffer
-#include <fstream>	//Open files
-#include <errno.h>	//Debugging
-#include <iostream>	//Print for debugging (Remove later?)
-#include <pthread.h>	//Threading for program
-#include <semaphore.h>	//Semaphores
-#include <sstream>	//String buffer
+#include <stdlib.h>		//Exit functions for incorrect files
+#include "time_functions.h"	//Timing
+#include <string>		//Buffer
+#include <fstream>		//Open files
+#include <errno.h>		//Debugging
+#include <iostream>		//Print for debugging (Remove later?)
+#include <pthread.h>		//Threading for program
+#include <semaphore.h>		//Semaphores
+#include <sstream>		//String buffer
 using namespace std;
 
 //Thread functions
 void *threadProducer(void * foo);
 
 void *threadConsumer(void * foo);
-
 
 
 //Global variables
@@ -40,6 +39,7 @@ string Que[10];			//Circular queue
 //	to run.
 int main(int argc, char * argv[]){
 
+double wall_time, cpu_time; //local variables to hold time
 
 //Open input file
 infile.open(argv[1]);
@@ -55,25 +55,76 @@ if(!outfile.good()){
 }
 
 //Init Semaphores
-int v1, v2, v3;
 sem_init(&Full,0, 0);
 sem_init(&Empty, 0, 10);
 sem_init(&Crit,0,1);
 
-
-clock_t cpuStart, cpuEnd;
-stuct timeb wStart, wEnd;
+//Timing
+start_timing();
 cout << "Starting timing: \n";
-ftime(&wStart); cpuStart = clock();
 
+//Create Producer and Consumer
+pthread_t tProducer; pthread_t tConsumer;
+pthread_create(&tProducer, NULL, threadProducer,NULL);
+pthread_create(&tConsumer, NULL, threadConsumer,NULL);
+
+pthread_join(tProducer, NULL); pthread_join(tConsumer, NULL); 
+
+stop_timing();
+wall_time = get_wall_clock_diff();
+cpu_time  = get_CPU_time_diff();
+cout << "The time it took as wall time is: " << wall_time << ".\n";
+cout << "The time it took as CPU time is: " << cpu_time << ".\n";
+
+//Close files, now finished
+infile.close(); outfile.close();
+
+//Destory Semaphores
+sem_destroy(&Crit); sem_destroy(&Empty); sem_destroy(&Full);
 return(0);
 }
 
 
-void threadConsumer(void * foo){
-
+void *threadConsumer(void * foo){
+	cout << "Entering Consumer.\n";
+	string temp;
+	int index = 0;
+	int count = 0;
+	temp = Que[index];
+	while(temp[0] != char(-1)){	//char 1 was chosen to be a character not in
+	sem_wait(&Full);		// the file to signal that its at the end of
+	sem_wait(&Crit);		// the program
+	temp = Que[index];
+	if(temp[0] != char(-1)){
+	outfile << temp << "\n";	//Write line
+	}
+	count++;
+	index = (index+1)%10;		//Increment queue, % to make circular
+	sem_post(&Crit);		//Release Crit
+	sem_post(&Empty);		//Signal empty space
+	}
+	return (void *)0;
 }
 
-void threadProducer(void * foo){
-
+void *threadProducer(void * foo){
+	cout << "Entering Producer\n";
+	string line;
+	int count = 0;
+	int index = 0;
+	
+	while(getline(infile, line)){
+	sem_wait(&Empty);
+	sem_wait(&Crit);		//wait for critical section	
+	Que[index] = line;		//Line in queue
+	count++;
+	index = (index+1)%10;		//Increment queue, % to make circular
+	sem_post(&Crit);		//Release Critical for Consumer
+	sem_post(&Full);		//Tell Consumer that there is info in queue
+	}
+	//Done getting lines in file
+	sem_wait(&Empty);		//Make sure room is in queue
+	Que[index] = char(-1);
+	sem_post(&Full);		//Make sure Consumer finishes first
+	return (void *)0;
 }
+
